b01f57d56d70bde4fc36f42e4fbc6995b14e9579
Daniel Pelsmaeker <info@virtlink.com>
2016-10-16 18:29:55 +0200

    Replace NETStandard.Library dependency by System.Collections

--- src/Virtlink.Utilib/Properties/AssemblyInfo.cs
+++ src/Virtlink.Utilib/Properties/AssemblyInfo.cs
@@ -16,4 +16,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c09681f7-8763-4481-a25c-3cd2f5906df9")]
+//[assembly: Guid("c09681f7-8763-4481-a25c-3cd2f5906df9")]
