f62cb9f0b08ace51f9456c14f0cd4ade1c924a55
Daniel Pelsmaeker <info@virtlink.com>
2016-10-17 23:12:17 +0200

    Add Enumerables.TryGetCount

---
+++ src/Virtlink.Utilib/Collections/Enumerables.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Virtlink.Utilib.Collections
+{
+    /// <summary>
+    /// Functions for working with enumerables.
+    /// </summary>
+    public static class Enumerables
+    {
+        /// <summary>
+        /// Attempts to get the number of elements in the enumerable,
+        /// only if it can be determined efficiently.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the enumerable.</typeparam>
+        /// <param name="enumerable">The enumerable for which tog et the count.</param>
+        /// <returns>The number of elements;
+        /// or <see langword="null"/> when it could not be determined
+        /// without enumerating all elements.</returns>
+        public static int? TryGetCount<T>(IEnumerable<T> enumerable)
+        {
+            #region Contract
+            if (enumerable == null)
+                throw new ArgumentNullException(nameof(enumerable));
+            #endregion
+
+            var collection = enumerable as ICollection<T>;
+            if (collection != null)
+                return collection.Count;
+
+            var roCollection = enumerable as IReadOnlyCollection<T>;
+            if (roCollection != null)
+                return roCollection.Count;
+
+            return TryGetCount((IEnumerable)enumerable);
+        }
+
+        /// <summary>
+        /// Attempts to get the number of elements in the enumerable,
+        /// only if it can be determined efficiently.
+        /// </summary>
+        /// <param name="enumerable">The enumerable for which tog et the count.</param>
+        /// <returns>The number of elements;
+        /// or <see langword="null"/> when it could not be determined
+        /// without enumerating all elements.</returns>
+        public static int? TryGetCount(IEnumerable enumerable)
+        {
+            #region Contract
+            if (enumerable == null)
+                throw new ArgumentNullException(nameof(enumerable));
+            #endregion
+
+            var collection = enumerable as ICollection;
+            if (collection != null)
+                return collection.Count;
+
+            return null;
+        }
+    }
+}
---
+++ test/Virtlink.Utilib.Tests/Collections/EnumerablesTests.TryGetCountTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Virtlink.Utilib.Collections
+{
+    partial class EnumerablesTests
+    {
+        /// <summary>
+        /// Tests the <see cref="Enumerables.TryGetCount"/> method.
+        /// </summary>
+        public sealed class TryGetCountTests
+        {
+            [Fact]
+            public void ReturnsCountOfICollection_T()
+            {
+                // Arrange
+                var collection = new List<String>{ "a", "b", "c" };
+
+                // Act
+                int? result = Enumerables.TryGetCount(collection);
+
+                // Assert
+                Assert.Equal(3, result);
+            }
+
+            [Fact]
+            public void ReturnsCountOfICollection()
+            {
+                // Arrange
+                var collection = new ArrayList { "a", "b", "c" };
+
+                // Act
+                int? result = Enumerables.TryGetCount(collection);
+
+                // Assert
+                Assert.Equal(3, result);
+            }
+
+            [Fact]
+            public void ReturnsNullForNonCollection()
+            {
+                // Arrange
+                var enumerable = new [] { "a", "b", "c" }.Select(s => s);
+
+                // Act
+                int? result = Enumerables.TryGetCount(enumerable);
+
+                // Assert
+                Assert.Null(result);
+            }
+        }
+    }
+}
