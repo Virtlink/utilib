d82437377f9359ce76b881557b73384b495d2a71
Daniel Pelsmaeker <info@virtlink.com>
2017-01-31 21:04:42 +0100

    Add List.Copy() function

--- src/Virtlink.Utilib/Collections/List.cs
+++ src/Virtlink.Utilib/Collections/List.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Virtlink.Utilib.Collections
 {
@@ -13,5 +14,57 @@ namespace Virtlink.Utilib.Collections
         /// <typeparam name="T">The type of elements in the list.</typeparam>
         /// <returns>The read-only empty list.</returns>
         public static IReadOnlyList<T> Empty<T>() => Arrays.Empty<T>();
+
+        /// <summary>
+        /// Copies elements from one list to another.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the lists.</typeparam>
+        /// <param name="sourceList">The source list.</param>
+        /// <param name="sourceIndex">The source index.</param>
+        /// <param name="destinationList">The destination list.</param>
+        /// <param name="destinationIndex">The destination index.</param>
+        /// <param name="count">The number of elements to copy.</param>
+        public static void Copy<T>(IReadOnlyList<T> sourceList, int sourceIndex, IList<T> destinationList,
+            int destinationIndex, int count)
+        {
+            #region Contract
+            if (sourceList == null)
+                throw new ArgumentNullException(nameof(sourceList));
+            if (sourceIndex < 0 || sourceIndex > sourceList.Count)
+                throw new ArgumentOutOfRangeException(nameof(sourceIndex));
+            if (destinationList == null)
+                throw new ArgumentNullException(nameof(destinationList));
+            if (destinationIndex < 0 || destinationIndex > destinationList.Count)
+                throw new ArgumentOutOfRangeException(nameof(destinationIndex));
+            if (count < 0 || sourceIndex + count > sourceList.Count || destinationIndex + count > destinationList.Count)
+                throw new ArgumentOutOfRangeException(nameof(count));
+            #endregion
+
+
+            if (sourceIndex < destinationIndex)
+            {
+                for (int i = count - 1; i >= 0; i--)
+                {
+                    destinationList[destinationIndex + i] = sourceList[sourceIndex + i];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    destinationList[destinationIndex + i] = sourceList[sourceIndex + i];
+                }
+            }
+        }
+
+        /// <summary>
+        /// Copies elements from one list to another.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the lists.</typeparam>
+        /// <param name="sourceList">The source list.</param>
+        /// <param name="destinationList">The destination list.</param>
+        /// <param name="count">The number of elements to copy.</param>
+        public static void Copy<T>(IReadOnlyList<T> sourceList, IList<T> destinationList, int count)
+            => Copy(sourceList, 0, destinationList, 0, count);
     }
 }
---
+++ test/Virtlink.Utilib.Tests/Collections/ListTests.CopyTests.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Virtlink.Utilib.Collections
+{
+    partial class ListTests
+    {
+        /// <summary>
+        /// Tests the <see cref="List.Copy"/> function.
+        /// </summary>
+        [TestFixture]
+        public sealed class CopyTests
+        {
+            [Test]
+            public void CountZero_CopiesNothing()
+            {
+                // Arrange
+                var source = new int[] {1, 2, 3};
+                var destination = new int[5];
+
+                // Act
+                List.Copy(source, 0, destination, 0, 0);
+
+                // Assert
+                Assert.That(destination, Is.EqualTo(new[] {0, 0, 0, 0, 0}));
+            }
+
+            [Test]
+            public void TwoLists_CopiesStuff()
+            {
+                // Arrange
+                var source = new int[] { 1, 2, 3 };
+                var destination = new int[] { 1, 3, 5, 7, 9 };
+
+                // Act
+                List.Copy(source, 1, destination, 2, 2);
+
+                // Assert
+                Assert.That(destination, Is.EqualTo(new[] { 1, 3, 2, 3, 9 }));
+            }
+
+            [Test]
+            public void SameListFromStartToLater_CopiesBackward()
+            {
+                // Arrange
+                var list = new int[] { 1, 2, 3, 4, 5 };
+
+                // Act
+                List.Copy(list, 1, list, 3, 2);
+
+                // Assert
+                Assert.That(list, Is.EqualTo(new[] { 1, 2, 3, 2, 3 }));
+            }
+
+            [Test]
+            public void SameListFromLaterToStart_CopiesForward()
+            {
+                // Arrange
+                var list = new int[] { 1, 2, 3, 4, 5 };
+
+                // Act
+                List.Copy(list, 3, list, 1, 2);
+
+                // Assert
+                Assert.That(list, Is.EqualTo(new[] { 1, 4, 5, 4, 5 }));
+            }
+
+            [Test]
+            public void SameListFromSameToSame_CopiesNothing()
+            {
+                // Arrange
+                var list = new int[] { 1, 2, 3, 4, 5 };
+
+                // Act
+                List.Copy(list, 2, list, 2, 2);
+
+                // Assert
+                Assert.That(list, Is.EqualTo(new[] { 1, 2, 3, 4, 5 }));
+            }
+
+            [Test]
+            public void NullSource_ThrowsException()
+            {
+                // Act/Assert
+                Assert.That(() => {
+                    List.Copy(null, 0, new int[5], 0, 0);
+                }, Throws.ArgumentNullException);
+            }
+
+            [Test]
+            public void NullDestination_ThrowsException()
+            {
+                // Act/Assert
+                Assert.That(() => {
+                    List.Copy(new int[5], 0, null, 0, 0);
+                }, Throws.ArgumentNullException);
+            }
+
+            [Test]
+            public void NegativeSourceIndex_ThrowsException()
+            {
+                // Act/Assert
+                Assert.That(() => {
+                    List.Copy(new int[5], -3, new int[5], 0, 0);
+                }, Throws.InstanceOf<ArgumentOutOfRangeException>());
+            }
+
+            [Test]
+            public void NegativeDestinationIndex_ThrowsException()
+            {
+                // Act/Assert
+                Assert.That(() => {
+                    List.Copy(new int[5], 0, new int[5], -3, 0);
+                }, Throws.InstanceOf<ArgumentOutOfRangeException>());
+            }
+
+            [Test]
+            public void NegativeCount_ThrowsException()
+            {
+                // Act/Assert
+                Assert.That(() => {
+                    List.Copy(new int[5], 0, new int[5], 0, -3);
+                }, Throws.InstanceOf<ArgumentOutOfRangeException>());
+            }
+
+            [Test]
+            public void SourceIndexOutOfBounds_ThrowsException()
+            {
+                // Act/Assert
+                Assert.That(() => {
+                    List.Copy(new int[5], 6, new int[5], 0, 0);
+                }, Throws.InstanceOf<ArgumentOutOfRangeException>());
+            }
+
+            [Test]
+            public void DestinationIndexOutOfBounds_ThrowsException()
+            {
+                // Act/Assert
+                Assert.That(() => {
+                    List.Copy(new int[5], 0, new int[5], 6, 0);
+                }, Throws.InstanceOf<ArgumentOutOfRangeException>());
+            }
+
+            [Test]
+            public void CountOutOfBounds_ThrowsException()
+            {
+                // Act/Assert
+                Assert.That(() => {
+                    List.Copy(new int[5], 5, new int[5], 0, 1);
+                }, Throws.InstanceOf<ArgumentOutOfRangeException>());
+            }
+        }
+    }
+}
