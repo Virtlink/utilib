8b7ee35aa8429121aba9683b20ae6a347713d6f7
Virtlink <info@virtlink.com>
2017-03-14 20:15:14 +0100

    Change from project.json to project.csproj

--- src/Virtlink.Utilib/Properties/AssemblyInfo.cs
+++ src/Virtlink.Utilib/Properties/AssemblyInfo.cs
@@ -1,10 +1,4 @@
-﻿using System.Reflection;
+﻿using System;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Virtlink.Utilib")]
-[assembly: AssemblyTrademark("")]
-[assembly: ComVisible(false)]
 [assembly: InternalsVisibleTo("Virtlink.Utilib.Tests")]
